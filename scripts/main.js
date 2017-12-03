8ba77916e16276cf39a27de39bb3db20428eb86e
takasago <takasago.kg@gmail.com>
2017-12-03 09:06:29 +0900

    Test release 171203-0905
    add optional query string 'id'

--- scripts/main.js
+++ scripts/main.js
@@ -73,6 +73,21 @@ function callTo(peerId){
 	});
 }
 
+function getUrlVars () {
+  var vars = [], max = 0, hash = "", array = "";
+  var url = window.location.search;
+  
+  hash  = url.slice(1).split('&');    
+  max = hash.length;
+  for (var i = 0; i < max; i++) {
+    array = hash[i].split('=');
+    vars.push(array[0]);
+    vars[array[0]] = array[1];
+  }
+  
+  return vars;
+}
+
 function getRandomString() {
   var BaseString ='abcdefghijklmnopqrstuvwxyz';
   
